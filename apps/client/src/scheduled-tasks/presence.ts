916e3182d906be99fd1cd949a51fdf457dd531a0
simxnet <simxnet@envs.net>
2023-07-02 22:12:03 +0200

    feat: latest changes

---
+++ apps/client/src/scheduled-tasks/presence.ts
@@ -0,0 +1,28 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { ScheduledTask } from "@sapphire/plugin-scheduled-tasks";
+import { isNullOrUndefined } from "@sapphire/utilities";
+import { ActivityType } from "discord.js";
+import { minutes } from "../lib/util/time";
+
+@ApplyOptions<ScheduledTask.Options>({
+	interval: minutes(5)
+})
+export class PresenceTask extends ScheduledTask {
+	public async run() {
+		// Get a quote from Kanye :P
+		const { quote } = (await (
+			await fetch("https://api.kanye.rest/")
+		).json()) as { quote: string };
+
+		if (isNullOrUndefined(quote))
+			return this.container.client.user?.setActivity({
+				name: "you",
+				type: ActivityType.Watching
+			});
+
+		return this.container.client.user?.setActivity({
+			name: quote.toLowerCase(),
+			type: ActivityType.Watching
+		});
+	}
+}
